7e87ba2f78960e3498660f812b154223dc0ad8c8
Dustin Yost <dustin.yost.t@gmail.com>
2018-08-17 11:25:45 -0400

    Remove router basename to avoid double-root route paths

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
 
-ReactDOM.render(<BrowserRouter basename={process.env.PUBLIC_URL}><App/></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(<BrowserRouter><App/></BrowserRouter>, document.getElementById('root'));
