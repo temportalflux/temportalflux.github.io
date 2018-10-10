e48d1bc042e61aab6592d2d6b22231b209c395b7
temportalflux <dustin.yost.t@gmail.com>
2018-10-10 16:00:38 -0400

    fix: Remove extra import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, Switch, withRouter} from "react-router-dom";
+import {Route, Switch} from "react-router-dom";
 import * as shortid from 'shortid';
 import {ROUTES} from "./Routes";
 import {Container} from "semantic-ui-react";
